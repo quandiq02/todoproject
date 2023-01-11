6b74e8251311c4929cccfce565f0b857643630cc
Quandiq <quandiqqalbaev2002@mail.ru>
2023-01-11 19:33:30 +0500

    eve 2

--- modules/notes/sortByID.js
+++ modules/notes/sortByID.js
@@ -1,24 +1,38 @@
 function sortByID() {
-  let _lsNotes = JSON.parse(localStorage.getItem("notes"));
+  let _lsNotes = JSON.parse(localStorage.getItem("notes")),
+    _lsUsers = JSON.parse(localStorage.getItem("users-list"));
   let _currentUSER = JSON.parse(localStorage.getItem("current-user"));
   let tasks = document.querySelectorAll(".task__item");
   let cats = document.querySelectorAll(".category__item");
   for (let i = 0; i < _lsNotes.length; i++) {
     if (_currentUSER[0].userID != _lsNotes[i].userID) {
-      let tasksHide = document.querySelectorAll(`.task__userID-${_lsNotes[i].userID}`);
-      for(let k=0;k<tasksHide.length;k++){
-        tasksHide[k].classList.add('hideOthersbyID')
+      let tasksHide = document.querySelectorAll(
+        `.task__userID-${_lsNotes[i].userID}`
+      );
+      for (let k = 0; k < tasksHide.length; k++) {
+        tasksHide[k].classList.add("hideOthersbyID");
+      }
     }
-    }      
   }
   for (let i = 0; i < _lsNotes.length; i++) {
     if (_currentUSER[0].userID != _lsNotes[i].userID) {
-      let catsHide = document.querySelectorAll(`.category__userID-${_lsNotes[i].userID}`);
-      for(let k=0;k<catsHide.length;k++){
-        catsHide[k].classList.add('hideOthersbyID')
+      let catsHide = document.querySelectorAll(
+        `.category__userID-${_lsNotes[i].userID}`
+      );
+      for (let k = 0; k < catsHide.length; k++) {
+        catsHide[k].classList.add("hideOthersbyID");
+      }
     }
-    }      
   }
- 
+  for (let i = 0; i < _lsUsers.length; i++) {
+    if (_lsUsers[i].isAdmin == true) {
+      for (let k = 0; k < _lsNotes.length; k++) {
+        let catsHide = document.querySelectorAll(`.category__item`);
+        let tasksHide = document.querySelectorAll(`.task__item`);
+        tasksHide[k].classList.remove('hideOthersbyID');
+        catsHide[k].classList.remove("hideOthersbyID");
+      }
+    }
+  }
 }
 export { sortByID };
